96fe3c82f2b6c7f51ca3059dd3e9616cc795e79f
Mehadi Hasan <mhshakil06@gmail.com>
2024-01-20 17:44:42 +0600

    Navbar and Sidebar added

--- src/Page/Dashboard/Home/Home/Home.jsx
+++ src/Page/Dashboard/Home/Home/Home.jsx
@@ -1,8 +1,11 @@
 import './Home.css'
+import Navbar from './../../../Navbar/Navbar';
+
 
 const Home = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <h1>this is home page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio doloribus sapiente fugiat qui tempora, eveniet ipsam temporibus vero officiis! Odio nihil voluptatum, consequatur porro neque, maiores adipisci similique vero accusamus autem sit doloremque quisquam! Ea exercitationem itaque facilis magnam laudantium aliquam culpa odio natus non tempore. Rem et quam nobis expedita? Voluptas voluptatum repellat delectus magni commodi explicabo ab, odit enim, et amet dolorem. Possimus dolorem fuga deleniti quaerat. Perspiciatis ut recusandae incidunt dolorum mollitia porro nihil. Accusantium voluptas ab quisquam officia aliquam ducimus nostrum reprehenderit cumque, magni, voluptates cupiditate eaque cum, officiis non laudantium placeat. At, cupiditate sunt! Deserunt iure iusto illo aspernatur laboriosam vel rerum dolore ex, ullam dignissimos fugit at quo, hic blanditiis tempore a veniam! Dolorem nostrum neque sequi dolore adipisci voluptate et? Consequatur velit voluptatem nobis quis eos. Explicabo repellat iste nulla. Maxime sunt nemo dolor! Aut officia id mollitia ad, culpa maxime, at porro vero hic excepturi numquam beatae perferendis molestiae optio quaerat distinctio impedit tempora voluptatum nisi dolor a ut quo voluptate deserunt. Earum eligendi, culpa a eaque exercitationem praesentium, voluptatem animi, delectus excepturi sequi saepe magnam neque voluptate laboriosam similique! Suscipit earum inventore placeat debitis tempora quia? Nihil ut quaerat quasi sapiente!</p>
     </div>
---
+++ src/Page/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+
+import Sidebar from './../Sidebar/Sidebar';
+function Navbar() {
+  return (
+    <>
+    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar-start">
+        {/* sidebar  */}
+        <Sidebar></Sidebar>
+    </div>
+    <div className="navbar-center">
+        <a className="btn btn-ghost text-xl">Holos-techies
+
+</a>
+    </div>
+    <div className="navbar-end">
+        <button className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        </button>
+
+        <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
