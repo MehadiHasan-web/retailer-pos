306eb8e9e611dacdabcdf8dbaf354e8af5045268
Mehadi Hasan <mhshakil06@gmail.com>
2024-01-22 17:29:11 +0600

    inventory history table design

--- src/Page/History/InventoryHistory.jsx
+++ src/Page/History/InventoryHistory.jsx
@@ -1,7 +1,13 @@
 import Title from "../../Title/Title"
+import  { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 function InventoryHistory() {
+
+    const [startDate, setStartDate] = useState(new Date());
+
   return (
     <>
     {/* title section start */}
@@ -9,164 +15,154 @@ function InventoryHistory() {
       {/* title section end */}
       {/* table history  */}
       <div >
-        <div className="container mx-auto">
+        <div className="container mx-auto px-12">
        <div className="flex justify-start my-3 ">
        <h2 className="w-34  font-semibold border-b-[1px] border-indigo-500 ">Inventory History:</h2> <span className="ms-2"> Branch Name</span>
        </div>
-        <div className="overflow-x-auto">
+
+            {/* search bar  */}
+            <div className="py-2 mb-3 bg-slate-100 rounded-lg">
+               <div className="flex justify-center mt-1">
+                <form action="">
+                    {/* category  */}
+                    <select className="select select-sm select-bordered w-44 max-w-xs rounded-full mx-1 mb-1 " >
+                        <option disabled selected>Category</option>
+                        <option>Han Solo</option>
+                        <option>Greedo</option>
+                    </select>
+                    {/* subcategory  */}
+                    <select className="select select-sm select-bordered w-44 max-w-xs rounded-full mx-1 mb-1 " >
+                        <option disabled selected>Subcategory?</option>
+                        <option>Han Solo</option>
+                        <option>Greedo</option>
+                    </select>
+                    {/* subcategory  */}
+                    {/* date end */}
+                    <select className="select select-sm select-bordered w-44 max-w-xs rounded-full mx-1 mb-1  " >
+                        <option disabled selected>Subcategory?</option>
+                        <option>Han Solo</option>
+                        <option>Greedo</option>
+                    </select>
+                    {/* date  */}
+                   
+                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className="w-32 overflow-hidden border input input-sm mx-1 mb-1 rounded-full" />
+                    
+                    {/* search bar  */}
+                    <input type="text" placeholder="Type here" className="input input-bordered input-sm max-w-xs w-52 rounded-full mx-1 mb-1 " />
+                    <button type="submit" className="btn btn-outline btn-sm rounded-full mx-3  hover:text-white ">Search</button>
+                    <button type="button" className="btn btn-outline btn-sm rounded-full mx-1 mb-1  hover:text-white ">Clear filter</button>
+
+                </form>
+               </div>
+            </div>
+            {/* search bar end  */}
+       
+        <div className="overflow-x-auto  shadow-lg rounded">
             <table className="table">
                 {/* head */}
-                <thead>
+                <thead className="bg-slate-200	">
                 <tr>
-                    <th>
+                    <th className="text-black">
                     Select
                     {/* <label>
                         <input type="checkbox" className="checkbox checkbox-sm" />
                     </label> */}
                     </th>
-                    <th>Name</th>
-                    <th>Job</th>
-                    <th>Quantity</th>
-                    <th>Action</th>
+                    <th  className="text-black">Name</th>
+                    <th  className="text-black">Request Date</th>
+                    <th  className="text-black">Perches Date</th>
+                    <th  className="text-black">Receive Date</th>
+                    <th  className="text-black">Quantity</th>
+                    <th  className="text-black">Action</th>
                 </tr>
                 </thead>
                 <tbody>
-                {/* row 1 */}
-                <tr>
-                    <th>
-                    <label>
-                        <input type="checkbox" className="checkbox checkbox-sm" />
-                    </label>
-                    </th>
-                    <td>
-                    <div className="flex items-center gap-3">
-                        <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Avatar Tailwind CSS Component" />
+                    {/* row 1 */}
+                    <tr>
+                        <th>
+                        <label>
+                            <input type="checkbox" className="checkbox checkbox-sm" />
+                        </label>
+                        </th>
+                        <td>
+                        <div className="flex items-center gap-3">
+                            <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Avatar Tailwind CSS Component" />
+                            </div>
+                            </div>
+                            <div>
+                            <div className="font-bold">Hart Hagerty</div>
+                            <div className="text-sm opacity-50">United States</div>
+                            </div>
                         </div>
+                        </td>
+                        <td>
+                            <p>20 january </p>
+                        </td>
+                        <td>
+                            <p>20 january </p>
+                        </td>
+                        <td>
+                            <p>20 january </p>
+                        </td>
+                        <td>4</td>
+                        <th>
+                        <button className="btn btn-outline btn-error btn-xs">Cenacle</button>
+                        </th>
+                    </tr>                
+                    {/* row 2 */}
+                    <tr>
+                        <th>
+                        <label>
+                            <input type="checkbox" className="checkbox checkbox-sm" />
+                        </label>
+                        </th>
+                        <td>
+                        <div className="flex items-center gap-3">
+                            <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Avatar Tailwind CSS Component" />
+                            </div>
+                            </div>
+                            <div>
+                            <div className="font-bold">Hart Hagerty</div>
+                            <div className="text-sm opacity-50">United States</div>
+                            </div>
                         </div>
-                        <div>
-                        <div className="font-bold">Hart Hagerty</div>
-                        <div className="text-sm opacity-50">United States</div>
-                        </div>
-                    </div>
-                    </td>
-                    <td>
-                    Zemlak, Daniel and Leannon
-                    <br/>
-                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                    </td>
-                    <td>4</td>
-                    <th>
-                    <button className="btn btn-outline btn-error btn-xs">Cenacle</button>
-                    </th>
-                </tr>
-                {/* row 2 */}
-                <tr>
-                    <th>
-                    <label>
-                        <input type="checkbox" className="checkbox checkbox-sm" />
-                    </label>
-                    </th>
-                    <td>
-                    <div className="flex items-center gap-3">
-                        <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Avatar Tailwind CSS Component" />
-                        </div>
-                        </div>
-                        <div>
-                        <div className="font-bold">Brice Swyre</div>
-                        <div className="text-sm opacity-50">China</div>
-                        </div>
-                    </div>
-                    </td>
-                    <td>
-                    Carroll Group
-                    <br/>
-                    <span className="badge badge-ghost badge-sm">Tax Accountant</span>
-                    </td>
-                    <td>1</td>
-                    <th>
-                    <button className="btn btn-outline btn-error btn-xs">Cenacle</button>
-                    </th>
-                </tr>
-                {/* row 3 */}
-                <tr>
-                    <th>
-                    <label>
-                        <input type="checkbox" className="checkbox checkbox-sm" /> 
-                    </label>
-                    </th>
-                    <td>
-                    <div className="flex items-center gap-3">
-                        <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Avatar Tailwind CSS Component" />
-                        </div>
-                        </div>
-                        <div>
-                        <div className="font-bold">Marjy Ferencz</div>
-                        <div className="text-sm opacity-50">Russia</div>
-                        </div>
-                    </div>
-                    </td>
-                    <td>
-                    Rowe-Schoen
-                    <br/>
-                    <span className="badge badge-ghost badge-sm">Office Assistant I</span>
-                    </td>
-                    <td>5</td>
-                    <th>
-                    <button className="btn btn-outline btn-error btn-xs">Cenacle</button>
-                    </th>
-                </tr>
-                {/* row 4 */}
-                <tr>
-                    <th>
-                    <label>
-                        <input type="checkbox" className="checkbox checkbox-sm " />
-                    </label>
-                    </th>
-                    <td>
-                    <div className="flex items-center gap-3">
-                        <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Avatar Tailwind CSS Component" />
-                        </div>
-                        </div>
-                        <div>
-                        <div className="font-bold">Yancy Tear</div>
-                        <div className="text-sm opacity-50">Brazil</div>
-                        </div>
-                    </div>
-                    </td>
-                    <td>
-                    Wyman-Ledner
-                    <br/>
-                    <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
-                    </td>
-                    <td>2</td>
-                    <th>
-                    <button className="btn btn-outline btn-error btn-xs">Cenacle</button>
-                    </th>
-                </tr>
+                        </td>
+                        <td>
+                            <p>20 january </p>
+                        </td>
+                        <td>
+                            <p>20 january </p>
+                        </td>
+                        <td>
+                            <p>20 january </p>
+                        </td>
+                        <td>4</td>
+                        <th>
+                        <button className="btn btn-outline btn-error btn-xs">Cenacle</button>
+                        </th>
+                    </tr>                
                 </tbody>
                 {/* foot */}
-                <tfoot>
+                <tfoot className="bg-slate-200	">
                 <tr>
-                    <th>
+                    <th  className="text-black">
                     <button className="btn btn-outline btn-sm">Delate</button>
                     </th>
-                    <th>Name</th>
-                    <th>Job</th>
-                    <th>Quantity</th>
-                    <th>Action</th>
+                    <th  className="text-black">Name</th>
+                    <th  className="text-black">Request Date</th>
+                    <th  className="text-black">Perches Date</th>
+                    <th  className="text-black">Receive Date</th>
+                    <th  className="text-black">Quantity</th>
+                    <th  className="text-black">Action</th>
                 </tr>
                 </tfoot>
                 
             </table>
-            </div>
+        </div>
         </div>
       </div>
     </>
