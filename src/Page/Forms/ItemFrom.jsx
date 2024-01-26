24e219f5abd7f58aebcba78f2d832d2b9814d41b
shuvapranta <shuvaprantadeb@gmail.com>
2024-01-26 20:11:19 +0600

    manage inventory created

---
+++ src/Page/Forms/ItemFrom.jsx
@@ -0,0 +1,88 @@
+const ItemFrom = () => {
+  return (
+    <div className="mt-10 ">
+      <div className=" p-5 rounded-lg shadow-md space-y-5 border-2 lg:w-2/5 mx-auto">
+        <h1 className="text-center text-xl font-bold mb-4">Item</h1>
+        <div className="card shadow-2xl bg-base-100">
+          <form className="card-body">
+            {/* Name field */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name:</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            {/* Category-id field */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Category-id:</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            {/* sub-category-id field */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Sub-category-id:</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            {/* image field */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Image:</span>
+              </label>
+              <input
+                type="file"
+                className="file-input w-full max-w-xs"
+                accept="image/*"
+              />
+            </div>
+            {/* File field */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">File:</span>
+              </label>
+              <input
+                type="file"
+                className="file-input w-full max-w-xs"
+                accept=".pdf,.doc,docx"
+              />
+            </div>
+            {/* stock field */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Stock:</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Create</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ItemFrom;
--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -7,6 +7,11 @@ import InventoryComplete from "../Page/Dashboard/InventoryComplete/InventoryComp
 import Admin from './../Page/Dashboard/Admin/Admin';
 import MyInventory from './../Page/My Inventory/MyInventory';
 import PurchaseRequest from './../Page/Purchase Request/PurchaseRequest';
+import ManageInventory from "../Page/ManageInventory/ManageInventory";
+import CategoryFrom from "../Page/Forms/CategoryFrom";
+import SubCategory from "../Page/Forms/SubCategory";
+import ItemFrom from "../Page/Forms/ItemFrom";
+import SubItemFrom from "../Page/Forms/SubItemFrom";
 
 
 const router = createBrowserRouter([
@@ -41,6 +46,33 @@ const router = createBrowserRouter([
       {
         path : '/purchaseRequest',
         element : <PurchaseRequest></PurchaseRequest>
+      },
+      {
+        path:"/manageInventory",
+        element:<ManageInventory></ManageInventory>,
+        children:[
+          {
+            path:"/manageInventory",
+            element:<CategoryFrom></CategoryFrom>
+
+          },
+          {
+            path:"/manageInventory/subcategory",
+            element:<SubCategory></SubCategory>
+
+          },
+          {
+            path:"/manageInventory/item",
+            element:<ItemFrom></ItemFrom>
+
+          },
+          {
+            path:"/manageInventory/subItem",
+            element:<SubItemFrom></SubItemFrom>
+
+          },
+          
+        ]
       }
     ]
   },
