85d86639ec8ca902cb613c1048bfae311b55732e
shuvapranta <shuvaprantadeb@gmail.com>
2024-03-30 21:28:05 +0600

    implement requirement

--- src/Page/PurchaseManagement/PurchaseManagement.jsx
+++ src/Page/PurchaseManagement/PurchaseManagement.jsx
@@ -8,10 +8,12 @@
 // }
 import { NavLink } from "react-router-dom";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { GiEntryDoor } from "react-icons/gi";
 
 import { RiMoneyDollarCircleFill } from "react-icons/ri";
+import { FaCodePullRequest } from "react-icons/fa6";
 
-const PurchaseManagement = () => {
+const PurchaseManagement = ({open}) => {
   return (
     <>
       {/* <div className="container mx-auto ">
@@ -77,30 +79,31 @@ const PurchaseManagement = () => {
     </Menu> */}
       <div className="collapse navlink p-0 ">
         <input type="checkbox" />
-        <div className="collapse-title lg:p-0 py-0 flex items-center text-white ">
-          <RiMoneyDollarCircleFill className="text-lg text-green-500 mr-2"/>
-          Purchase Management
+        <div title="Sales" className={`collapse-title lg:p-0 py-0 flex items-center  ${!open ? "justify-start" : "justify-end"} text-white `}>
+          <RiMoneyDollarCircleFill className={`text-2xl text-green-500 ${!open && "mr-2"}`}/>
+         {!open && <span>Sales</span>}
           <ChevronDownIcon
-            className="h-5 w-5 text-green-500 ml-5"
+            className={`h-5 w-5 text-green-500 ${!open && "ml-5"}`}
             aria-hidden="true"
           />
         </div>
         <div className="collapse-content p-0 w-full text-white  pt-2">
           <ul className="space-y-2 pl-5">
-            <li>
+            <li title="Sales Entry">
               <NavLink
-                to="/purchaseManagement/purchaseRequest"
-                className="block"
+                to="sales/sales-entry"
+                className="flex items-center"
               >
-                Purchase Request
+                <GiEntryDoor className="text-xl text-green-500 mr-2"/>
+                {!open && <span>Sales Entry</span>}
               </NavLink>
             </li>
-            <li>
+            <li title="Sales Request"> 
               <NavLink
-                to="/purchaseManagement/purchaseRequestHistory"
-                className="block"
-              >
-                Purchase Request History
+                to="sales/sales-request"
+                className="flex items-center"
+              ><FaCodePullRequest className="text-xl text-green-500 mr-2"/>
+              {!open && <span>Sales Request</span>}
               </NavLink>
             </li>
           </ul>
