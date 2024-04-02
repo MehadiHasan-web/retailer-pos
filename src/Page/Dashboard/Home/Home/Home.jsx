4c5e0f21ca67fa618c4e588a623e96529356c0eb
MH Shakil <mhshakil06@gmail.com>
2024-04-02 22:15:40 +0600

    add image

--- src/Page/Dashboard/Home/Home/Home.jsx
+++ src/Page/Dashboard/Home/Home/Home.jsx
@@ -24,7 +24,7 @@ import { Navigation, Pagination } from "swiper/modules";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
-  const { baseURL, searchData ,searchFun} = useContext(AuthContext);
+  const { baseURL, searchData, searchFun } = useContext(AuthContext);
   const initialCardTable = JSON.parse(localStorage.getItem("wishlist")) || [];
   const [wishlist, setWishlist] = useState(initialCardTable);
   const [open, setOpen] = useState(false);
@@ -220,11 +220,11 @@ const Home = () => {
                 See all
               </li> */}
               <li className="mt-2 sm:mt-0">
-            <label className="input input-bordered flex items-center gap-2">
-              <input type="text" className="grow" placeholder="Search" onChange={(e)=>{searchFun(e.target.value)}} />
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-10 h-10 p-1 opacity-70 bg-green-600 -mr-3 rounded-lg text-white"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
-            </label>
-          </li>
+                <label className="input input-bordered flex items-center gap-2">
+                  <input type="text" className="grow" placeholder="Search" onChange={(e) => { searchFun(e.target.value) }} />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-10 h-10 p-1 opacity-70 bg-green-600 -mr-3 rounded-lg text-white"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                </label>
+              </li>
             </ul>
             {/* products  list*/}
             <div className="mt-8">
@@ -293,7 +293,7 @@ const Home = () => {
                           <figure>
                             <img
                               className="w-68 h-40"
-                              src={bottol}
+                              src={'https://rpos.pythonanywhere.com/' + product?.invImage}
                               alt="Movie"
                             />
                           </figure>
