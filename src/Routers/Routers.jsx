a52fe0998bd68495e212211597e75f36785aa54d
shuvapranta <shuvaprantadeb@gmail.com>
2024-04-02 16:37:15 +0600

    damping code remove

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -3,34 +3,17 @@ import LogIn from "../Page/LogIn/LogIn";
 import DashBoard from './../Layout/Dashboard/Dashboard';
 import Home from "../Page/Dashboard/Home/Home/Home";
 import PrivateRoute from "./PrivateRoute";
-import BranchName from './../Page/InstituteManagementForm/BranchName';
-import InstituteName from './../Page/InstituteManagementForm/InstituteName';
-import DepartmentName from './../Page/InstituteManagementForm/DepartmentName';
-import CategoryFrom from './../Page/Forms/CategoryFrom';
-import SubCategory from './../Page/Forms/SubCategory';
-import ItemFrom from './../Page/Forms/ItemFrom';
-import SubItemFrom from './../Page/Forms/SubItemFrom';
 import CreateUser from "../Page/CreateUser/CreateUser";
 import InventoryRequest from "../Page/Dashboard/InventoryRequest/InventoryRequest";
-import Appointments from "../Page/Dashboard/Appointments/Appointments";
-// import PurchaseRequestHistory from "../Page/PurchaseManagement/PurchaseRequestHistory";
-// import PurchaseRequest from "../Page/PurchaseManagement/PurchaseRequest";
-import MyInventory from './../Page/My Inventory/MyInventory';
 import ErrorPage from "../Page/ErrorPage/ErrorPage";
-import AllUserInventory from "../Page/AllUserInventory/AllUserInventory";
 import UserInventory from "../Page/UserInventory/UserInventory";
-import HomeExtra from './../Page/Dashboard/Home/Home/HomeExtra';
-// import Practice from "../Page/Practice/Practice";
 import Dashboard from "../Page/Practice/Dashboard";
-import MyInventoryRequest from "../Page/MyInventoryRequest/MyInventoryRequest";
 import UserProfile from "../Page/UserProfile/UserProfile";
-import InventoryRequestDetails from "../Page/Dashboard/InventoryRequest/InventoryRequestDetails";
 import Invoice from "../Page/Invoice/Invoice";
 import SalesRequest from "../Page/SalesRequest/SalesRequest";
 import Expenses from "../Page/Expenses/Expenses";
 import Scanner from "../Page/Scanner";
 import AddInventoryProduct from "../Page/AddInventoryProduct";
-import SalesReturn from './../Page/SalesReturn/SalesReturn';
 import NotAllow from "../Page/NotAllow/NotAllow";
 
 
@@ -62,23 +45,6 @@ const router = createBrowserRouter([
         element: <PrivateRoute><Invoice></Invoice></PrivateRoute>
       },
       {
-        path: `/inventoryRequest/:id`,
-        element: <PrivateRoute><InventoryRequestDetails></InventoryRequestDetails></PrivateRoute>
-
-      },
-      {
-        path: '/applications',
-        element: <PrivateRoute><Appointments></Appointments></PrivateRoute>
-      },
-      {
-        path: '/myInventory',
-        element: <PrivateRoute><MyInventory></MyInventory></PrivateRoute>
-      },
-      {
-        path: '/my-inventory-request',
-        element: <PrivateRoute><MyInventoryRequest></MyInventoryRequest></PrivateRoute>
-      },
-      {
         path: 'sales/sales-request/sales-entry',
         element: <PrivateRoute><Home></Home></PrivateRoute>
       },
@@ -91,42 +57,7 @@ const router = createBrowserRouter([
         element: <PrivateRoute><Invoice></Invoice></PrivateRoute>
 
       },
-      {
-        path: "/manageInventory/subcategory",
-        element: <PrivateRoute><SubCategory></SubCategory></PrivateRoute>
-      },
-      {
-        path: "/manageInventory/category",
-        element: <PrivateRoute><CategoryFrom></CategoryFrom></PrivateRoute>
-
-      },
-
-      {
-        path: "/manageInventory/item",
-        element: <PrivateRoute><ItemFrom></ItemFrom></PrivateRoute>
-
-      },
-      {
-        path: "/manageInventory/subItem",
-        element: <PrivateRoute><SubItemFrom></SubItemFrom></PrivateRoute>
-
-      },
-      {
-        path: '/manageInventory/allUserInventory',
-        element: <PrivateRoute><AllUserInventory></AllUserInventory></PrivateRoute>
-      },
-      {
-        path: '/instituteManagement/instituteName',
-        element: <PrivateRoute><InstituteName></InstituteName></PrivateRoute>
-      },
-      {
-        path: '/instituteManagement/branchName',
-        element: <PrivateRoute><BranchName></BranchName></PrivateRoute>
-      },
-      {
-        path: '/instituteManagement/departmentName',
-        element: <PrivateRoute><DepartmentName></DepartmentName></PrivateRoute>
-      },
+      
       {
         path: '/createUser',
         element: <PrivateRoute><CreateUser></CreateUser></PrivateRoute>
@@ -136,10 +67,7 @@ const router = createBrowserRouter([
         path: '/management',
         element: <PrivateRoute><UserInventory></UserInventory></PrivateRoute>
       },
-      {
-        path: '/homeExtra',
-        element: <PrivateRoute><HomeExtra></HomeExtra></PrivateRoute>
-      },
+      
       {
         path: '/expenses',
         element: <PrivateRoute><Expenses></Expenses></PrivateRoute>
