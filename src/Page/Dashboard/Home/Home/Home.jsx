899c4be106f9ff8148e966001f7678a98a0f37e4
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-04-21 11:02:15 +0600

    addin unit

--- src/Page/Dashboard/Home/Home/Home.jsx
+++ src/Page/Dashboard/Home/Home/Home.jsx
@@ -31,9 +31,15 @@ const Home = () => {
   const [activeButton, setActiveButton] = useState("All");
   const [categories, setCategories] = useState([]);
   const [selectedProduct, setSelectedProduct] = useState([]);
+  const [value, setValue] = useState(null)
   const token = localStorage.getItem("token");
+  
+  function funData(unit, id){
+    setValue({unit, id})
+  }
 
-  console.log(selectedProduct)
+  console.log(value)
+
 
 
   const clearData = () => {
@@ -317,6 +323,10 @@ const Home = () => {
                               <p className="text-base font-bold">
                                 {product?.unit} Available
                               </p>
+                              <p className="text-base font-bold">
+                                Unit : {product?.id === value?.id ? value?.unit : 0}
+                              </p>
+                              
                             </li>
                           </ul>
                           <div></div>
@@ -328,7 +338,7 @@ const Home = () => {
                         <div className="flex flex-wrap items-center gap-4 justify-center m-5">
                           {product.variants?.map((item, index) => (
                             <div className="flex items-center gap-2" key={index}>
-                              <input type="radio" name="size"  value={item.size} id="size" className="radio radio-success" />
+                              <input type="radio" name="size" onChange={() => funData(item.unit, product.id)} value={item.size} id="size" className="radio radio-success" />
                               <label htmlFor="size" className="text-lg font-semibold">{item.size}</label>
                             </div>
                           ))}
