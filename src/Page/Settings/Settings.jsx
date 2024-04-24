7587dc592323de1d15872f95f89a18deb06e9222
MH Shakil <mhshakil06@gmail.com>
2024-04-24 19:37:03 +0600

    add custom size in settings page

--- src/Page/Settings/Settings.jsx
+++ src/Page/Settings/Settings.jsx
@@ -5,16 +5,14 @@ import { FaTrash } from "react-icons/fa";
 import { LuArrowLeft } from "react-icons/lu";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import { VscCloseAll } from "react-icons/vsc";
+import { TfiSave } from "react-icons/tfi";
 
 const Settings = () => {
 
   const token = localStorage.getItem("token");
+  const [size, setSize] = useState([])
 
-  const [toggleBtn, setToggleBtn] = useState(false);
-  const [customBtn, setCustomBtn] = useState(false);
-  const [sizeName, setSizeName] = useState('');
-  const [sizeQuantity, setSizeQuantity] = useState('');
-  const [totalSize, setTotalSize] = useState();
 
   const passChgFun = (event) => {
     event.preventDefault();
@@ -34,50 +32,95 @@ const Settings = () => {
       });
   }
 
-   // custom size and add quantity 
-   function addNameQuantity() {
-    const sizeObject = { ...totalSize, [`"${sizeName}"`]: parseInt(sizeQuantity) };
-    // const sizeArray = Object.entries(sizeObject);
-    setTotalSize(sizeObject)
-    // console.log(typeof (totalSize))
-    // console.log(sizeObject)
-  }
 
-  //add default size quantity
-  function defaultSize(value, name) {
-    const sizeObject = { ...totalSize, [`"${name}"`]: parseInt(value) };
-    setTotalSize(sizeObject)
-  }
+  // custome size
+  function addCustomSize(e) {
+    e.preventDefault()
+    var form = e.target;
+    console.log(form.name.value)
 
-   //delete size
-  function sizeDelete(key) {
-    const newSizeObject = { ...totalSize };
-    delete newSizeObject[key];
-    setTotalSize(newSizeObject);
+    setSize(prevSizes => {
+      const isTitleDifferent = prevSizes.every(size => size.title !== form.name.value);
 
-  }
+      if (isTitleDifferent) {
+        const newObject = {
+          title: form.name.value,
+          sizes: []
+        };
 
-  const sizeBtn = (event) => {
-    event.preventDefault();
-    const formData = new FormData();
-    const camelCaseValue = toggleBtn ? "True" : "False";
-    formData.append('is_variant', camelCaseValue)
-
-    const cleanedTotalSize = {};
-    Object.keys(totalSize).forEach(key => {
-      const cleanedKey = key.replace(/"/g, '');
-      cleanedTotalSize[cleanedKey] = totalSize[key];
+        return [...prevSizes, newObject];
+      } else {
+        toast.info(form.name.value + ' already exists');
+        return prevSizes;
+      }
     });
+  }
 
-    if (toggleBtn) {
-      const totalSizeString = JSON.stringify(cleanedTotalSize);
-      formData.append('unit_per_size', totalSizeString);
-    } else {
-      console.log('is_variant :' + toggleBtn)
+  // add sizes
+  function addSize(index, e) {
+    e.preventDefault()
+    var form = e.target;
+    setSize(prevItems => {
+      return prevItems.map((item, i) => {
+        if (i === index) {
+          console.log(item)
+
+          return {
+            ...item,
+            sizes: [...item.sizes, form.name.value]
+          };
+        }
+        return item;
+      });
+    });
+  }
+
+  // delete size 
+  const deleteSize = (itemIndex, sizeIndex) => {
+    setSize(prevItems => {
+      return prevItems.map((item, i) => {
+        if (i === itemIndex) {
+          // Filter out the size to delete from the sizes array
+          const updatedSizes = item.sizes.filter((size, index) => index !== sizeIndex);
+          return {
+            ...item,
+            sizes: updatedSizes // Update the sizes array with the filtered sizes
+          };
+        }
+        return item; // Return unchanged items
+      });
+    });
+  };
+
+  // remove items 
+  const removeItem = (indexToRemove) => {
+    setSize(prevItems => {
+      return prevItems.filter((item, index) => index !== indexToRemove);
+    });
+  };
+
+  // add sizes
+  function saveItems(index) {
+    const item = size[index];
+    if (item) {
+      // send backend
+      axios.post(`https://rpos.pythonanywhere.com/api/v1/variant-settings/`, item, {
+        headers: { 'Authorization': 'token ' + token }
+      })
+        .then(response => {
+          console.log('Response:', response.data);
+          toast.success("Successfully Saved");
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          toast.error(`${error.message} .Try again`);
+        });
+      // end send backend
+
     }
 
-    console.log(totalSize)
   }
+
 
 
   return (
@@ -128,129 +171,44 @@ const Settings = () => {
             </div>
           </div>
         </div>
-        {/* size form section start */}
-        <div className="w-full h-full bg-white p-2 mt-5">
+        {/* add costume size  */}
+        <div className=" h-full bg-white p-2 mt-5 w-4/6">
           <h1 className="text-sm sm:text-base md:text-lg lg:text-2xl font-bold my-2">Add Default Size</h1>
-          <form onSubmit={sizeBtn}>
-            <div className={`w-full md:mt-8 md:mb-5 border-[1px] ${toggleBtn === true ? 'border-green-400' : 'border-gray-200'}  rounded-md p-2`}>
-                <div className="flex justify-between items-center w-full my-2">
-                  <p className={`${toggleBtn === true ? 'text-black-500' : 'text-slate-400'}`}>Product Size & Quantity</p>
-                  <input type="checkbox" className="toggle" onClick={() => setToggleBtn(!toggleBtn)} />
+          <div className="border border-green-400 rounded-md p-2">
+            <form className="flex justify-between" onSubmit={addCustomSize}>
+              <input name="name" type="text" placeholder="Type custom" className="input input-bordered input-accent w-full max-w-xs input-sm rounded-full" />
+              <button type="submit" className="btn btn-neutral rounded-full btn-sm">Add</button>
+            </form>
+          </div>
+          {/* show size list  */}
+          {size.map((item, index) => (
+            <div key={index} className="mt-4 border  border-purple-400 rounded-md p-2">
+              <div className="flex justify-between">
+                <h4 className="font-bold text-purple-700	">{item?.title}</h4>
+                <form className="flex justify-between gap-2" onSubmit={(e) => addSize(index, e)}>
+                  <input name="name" type="text" placeholder="Type custom" className="input input-bordered border-purple-700 w-full max-w-xs input-sm rounded-full" />
+                  <button type="submit" className="inline-flex items-center rounded-full bg-purple-50 px-2 py-1 text-xs font-bold text-purple-700 ring-1 ring-inset ring-purple-700/10">Add</button>
+                </form>
+              </div>
+              <div className="flex justify-between">
+                <div>
+                  {item.sizes.map((size, sizeIndex) => (
+                    <div key={sizeIndex} className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10 my-2 mx-2  product-custom-size">{size} <button type="button" className="ms-2" onClick={() => deleteSize(index, sizeIndex)} ><FaTrash className="text-red-400 hover:text-red-800 text-xl" /></button>  </div>
+                  ))}
                 </div>
-                <div className={`w-full md:mt-5 md:mb-5 border-[1px] ${customBtn === true ? 'border-green-400' : 'border-gray-200'} rounded-md p-2 ${toggleBtn === true ? 'block' : 'hidden'}`}>
-
-                  <div className="flex justify-between items-center w-full my-2 ">
-                    <p className={`text-sm ${customBtn === true ? 'text-black-500' : 'text-slate-400'}`}>Default Size</p>
-                    <input type="checkbox" className="toggle toggle-sm" onClick={() => setCustomBtn(!customBtn)} />
-                  </div>
-                  <div className={`w-full ${customBtn === true ? 'block' : 'hidden'}`}>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 w-full">
-                      {/* XS  */}
-                      <div className="form-control w-full relative">
-                        <input
-                          type="number"
-                          placeholder="XS"
-                          className="input input-bordered input-sm w-full"
-                          onChange={(e) => defaultSize(e.target.value, 'XS')}
-                        />
-                        <button type="button" className="btn btn-xs hover:bg-green-400 bg-green-400 absolute right-2 top-1 rounded-full text-white">Add</button>
-                      </div>
-                      {/*  */}
-                      {/* S */}
-                      <div className="form-control w-full relative">
-                        <input
-                          type="number"
-                          placeholder="S"
-                          className="input input-bordered input-sm w-full"
-                          onChange={(e) => defaultSize(e.target.value, 'S')}
-                        />
-                        <button type="button" className="btn btn-xs hover:bg-green-400 bg-green-400 absolute right-2 top-1 rounded-full text-white">Add</button>
-                      </div>
-                      {/*  */}
-                      {/*  */}
-                      <div className="form-control w-full relative">
-                        <input
-                          type="number"
-                          placeholder="M"
-                          className="input input-bordered input-sm w-full"
-                          onChange={(e) => defaultSize(e.target.value, 'M')}
-                        />
-                        <button type="button" className="btn btn-xs hover:bg-green-400 bg-green-400 absolute right-2 top-1 rounded-full text-white">Add</button>
-                      </div>
-                      {/*  */}
-                      {/*L  */}
-                      <div className="form-control w-full relative">
-                        <input
-                          type="number"
-                          placeholder="L"
-                          className="input input-bordered input-sm w-full"
-                          onChange={(e) => defaultSize(e.target.value, 'L')}
-                        />
-                        <button type="button" className="btn btn-xs hover:bg-green-400 bg-green-400 absolute right-2 top-1 rounded-full text-white">Add</button>
-                      </div>
-                      {/*  */}
-                      {/*  */}
-                      <div className="form-control w-full relative">
-                        <input
-                          type="number"
-                          placeholder="XL"
-                          className="input input-bordered input-sm w-full"
-                          onChange={(e) => defaultSize(e.target.value, 'XL')}
-                        />
-                        <button type="button" className="btn btn-xs hover:bg-green-400 bg-green-400 absolute right-2 top-1 rounded-full text-white">Add</button>
-                      </div>
-                      {/*  */}
-                      {/*  */}
-                      <div className="form-control w-full relative">
-                        <input
-                          type="number"
-                          placeholder="XXL"
-                          className="input input-bordered input-sm w-full"
-                          onChange={(e) => defaultSize(e.target.value, 'XXL')}
-                        />
-                        <button type="button" className="btn btn-xs hover:bg-green-400 bg-green-400 absolute right-2 top-1 rounded-full text-white">Add</button>
-                      </div>
-                      {/*  */}
-                    </div>
-                  </div>
+                <div className="flex gap-2">
+                  <button onClick={() => saveItems(index)} type="button" className="inline-flex items-center rounded-full bg-blue-50 px-2 py-2 mt-2 text-xs font-bold text-blue-700 ring-1 ring-inset ring-blue-700/10 hover:font-2xl"><TfiSave className="text-lg" /></button>
+                  <button onClick={() => removeItem(index)} type="button" className="inline-flex items-center rounded-full bg-red-50 px-2 py-2 mt-2 text-xs font-bold text-red-700 ring-1 ring-inset ring-red-700/10 hover:font-2xl"><VscCloseAll className="text-xl" /></button>
                 </div>
-                <div className={`grid grid-cols-1 md:grid-cols-3 gap-2 w-full ${toggleBtn === true ? 'block' : 'hidden'}`}>
-                  <div className="form-control w-full">
-                    <label htmlFor="sizeName">Size Name</label>
-                    <input
-                      onChange={(event) => setSizeName(event.target.value)}
-                      type="text"
-                      placeholder="Size name"
-                      className="input input-bordered  w-full my-2 "
-                    />
-                  </div>
-                  {/* Quantity */}
-                  <div className="form-control w-full">
-                    <label htmlFor="size">Quantity</label>
-                    <input
-                      onChange={(event) => setSizeQuantity(event.target.value)}
-                      type="number"
-                      placeholder="Quantity"
-                      className="input input-bordered  w-full my-2 "
-                    />
-                  </div>
-                  <div className="form-control w-full md:mt-4">
-                    <button type="submit" className="bg-green-500 text-white px-4 py-3 w-full rounded hover:bg-green-600 mt-4" onClick={addNameQuantity}>Add</button>
-                  </div>
-                </div>
-                <div className="flex">
-                  {totalSize ? (
-                    Object.keys(totalSize).map((key) => (
-                      <div className="badge badge-outline me-2 px-[6px] rounded  font-bold mt-2 flex product-custom-size" key={key}>{`${key} : ${totalSize[key]}`} <button type="button" className="ms-2" onClick={() => sizeDelete(key)} ><FaTrash className="text-red-400 hover:text-red-800 text-xl" /></button>  </div>
-                    ))) : (
-                    <p className={`${toggleBtn === true ? 'block' : 'hidden'}`} >No size available.</p>
-                  )}
-                </div>
-
+              </div>
             </div>
-          </form>
+          ))
+
+          }
+
         </div>
-        {/* size form section end */}
+
+
       </div>
       <ToastContainer></ToastContainer>
     </div>
